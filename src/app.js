d8a590cce07478175b6986f5538b92a1371c7dda
Sayantan Chowdhury <shyuhaspivot@gmail.com>
2025-08-07 01:32:53 +0530

    middleware and env changes

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+
+//cors is who can talk to your application
+
+
+app.use(
+ cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+ })
+)
+
+
+//common middleware
+app.use(express.json({limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb"}));
+app.use(express.static("public"));
+
+const app = express();
+
+
+
+export  { app }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Hey dude from backend!!");
+import { app } from "./app.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./.env"
+})
+
+//we are listening to the app and the port
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
