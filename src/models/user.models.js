cd8fec8ac2c886244af15bd0fb1a3afc4a75673b
Sayantan Chowdhury <shyuhaspivot@gmail.com>
2025-08-09 17:41:37 +0530

    bycrypt addition and JWT token for authentication purposes

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -12,8 +12,10 @@ createdAt Date
 updatedAt Date
 */
 
+import bcrypt from "bcrypt";
 import { type } from "express/lib/response";
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
 //destructure the schema so that we dont have to revisit it
 
 const userSchema = new Schema(
@@ -68,9 +70,64 @@ const userSchema = new Schema(
 // dont need to add id because mongoose already handles that 
 // mongoose adds in between layers to validate the data 
 
+
+userSchema.pre("save", async function (next) {
+
+    if(!this.modified("password")) return next ()
+
+    //the password should only be modified at the time of save
+    //or at the time of update, not any time else hence we exit 
+
+    this.password = bcrypt.hash(this.password, 12);
+
+    //we are encrypting the password before it saves in db 12 meaning 12 
+    // rounds of hashing so that the password is more secure
+
+    next()
+})
+
+//standard procedure is to add next to pass it along the next middleware
+// this is for the pre save middleware
+// we are using this to make sure that the password is hashed before we save it to the database
+// we are using the bcryptjs library to hash the password
+
+
+userSchema.methods.isPasswordCorrect = async function(password){
+   return bcrypt.compare(password,  this.password)
+     //this is the password that we are checking against the hashed password    
+}
+
+//JWT tokens
+
+userSchema.methods.generateAccessToken = function () {
+    jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullname: this.fullName,
+
+    },
+     process.env.ACCESS_TOKEN_SECRET, { 
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+    );
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    jwt.sign({
+        _id: this._id,
+    },
+     process.env.REFRESH_TOKEN_SECRET, { 
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+    );
+}
+
+//short lived access token (for authenticating sign ins)
+
+
 export const User = mongoose.model("User", userSchema);
 
 //we are creating a document with the name user and 
 // the schema we created above and use mongoose feature
 //we will mention the name of schema that we will use in the doc
 //This is a variable designed with Mongoose
+
